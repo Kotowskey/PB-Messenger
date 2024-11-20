adc639a0d5815fb71e93e1d7c7638cbe658daa1e
Łukasz Kotowski <85959@student.pb.edu.pl>
2024-11-20 21:02:59 +0100

    początkowa implementacja signup

--- app/src/main/java/projekt/pb/sm/SignUpActivity.java
+++ app/src/main/java/projekt/pb/sm/SignUpActivity.java
@@ -1,22 +1,48 @@
 package projekt.pb.sm;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import projekt.pb.sm.databinding.ActivitySignUpBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import android.app.ProgressDialog;
+import android.view.View;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    ActivitySignUpBinding binding;
+
+    private FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+
+        progressDialog = new ProgressDialog(SignUpActivity.this);
+        progressDialog.setTitle("Creating Account");
+        progressDialog.setMessage("We're creating your account");
+
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() { // Usunięty średnik
+            @Override
+            public void onClick(View v) {
+                if (!binding.txtUsername.getText().toString().isEmpty() && !binding.txtEmail.getText().toString().isEmpty() && !binding.txtPassword.getText().toString().isEmpty()) {
+                    // Logika rejestracji użytkownika
+                } else {
+                    Toast.makeText(SignUpActivity.this, "Please enter your username", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }); // Dodany zamykający nawias
     }
 }
