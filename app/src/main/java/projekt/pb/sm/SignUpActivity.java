154d6f31fdaa1bf3bf2271070b0617c7d1b1815a
Łukasz Kotowski <85959@student.pb.edu.pl>
2024-11-28 19:59:59 +0100

    połączenie z firebase

--- app/src/main/java/projekt/pb/sm/SignUpActivity.java
+++ app/src/main/java/projekt/pb/sm/SignUpActivity.java
@@ -3,6 +3,11 @@ package projekt.pb.sm;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import projekt.pb.sm.databinding.ActivitySignUpBinding;
+import projekt.pb.sm.models.Users;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
@@ -25,6 +30,7 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
@@ -34,15 +40,36 @@ public class SignUpActivity extends AppCompatActivity {
         progressDialog.setTitle("Creating Account");
         progressDialog.setMessage("We're creating your account");
 
-        binding.btnSignUp.setOnClickListener(new View.OnClickListener() { // Usunięty średnik
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!binding.txtUsername.getText().toString().isEmpty() && !binding.txtEmail.getText().toString().isEmpty() && !binding.txtPassword.getText().toString().isEmpty()) {
-                    // Logika rejestracji użytkownika
-                } else {
+                if (!binding.txtUsername.getText().toString().isEmpty() && !binding.txtEmail.getText().toString().isEmpty() && !binding.txtPassword.getText().toString().isEmpty())
+                {
+                    progressDialog.show();
+                    mAuth.createUserWithEmailAndPassword(binding.txtEmail.getText().toString(), binding.txtPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(Task<AuthResult> task) {
+                            if (task.isSuccessful())
+                            {
+                                Users user = new Users(binding.txtUsername.getText().toString(), binding.txtEmail.getText().toString(), binding.txtPassword.getText().toString());
+                                String id = task.getResult().getUser().getUid();
+                                database.getReference().child("Users").child(id).setValue(user);
+                                progressDialog.dismiss();
+                                Toast.makeText(SignUpActivity.this, "Account created successfully", Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+                                progressDialog.dismiss();
+                                Toast.makeText(SignUpActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+                else
+                {
                     Toast.makeText(SignUpActivity.this, "Please enter your username", Toast.LENGTH_SHORT).show();
                 }
             }
-        }); // Dodany zamykający nawias
+        });
     }
 }
