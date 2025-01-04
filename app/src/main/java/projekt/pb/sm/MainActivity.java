604b2c5b0d5dacd20de3a6cb4520e7d7d3936327
Kotowskey <122537577+Kotowskey@users.noreply.github.com>
2025-01-04 15:51:13 +0100

    wylogowywanie

--- app/src/main/java/projekt/pb/sm/MainActivity.java
+++ app/src/main/java/projekt/pb/sm/MainActivity.java
@@ -1,23 +1,20 @@
 package projekt.pb.sm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
-
-import androidx.activity.EdgeToEdge;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
-
 import projekt.pb.sm.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
-    FirebaseAuth mAuth;
+    private ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +22,38 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        setSupportActionBar(binding.toolbar);
+
         mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.settings) {
+            Toast.makeText(this, "Settings is clicked", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.groupChat) {
+            Toast.makeText(this, "Group Chat is clicked", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.logout) {
+            mAuth.signOut();
+            Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
